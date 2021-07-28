fc0fa01aa2261a3a25889fe3c4ee8b514b2c94de
Kusch Lionel <lionel.kusch@univ-amu.fr>
2021-07-28 12:21:00 +0200

    Add barrier before the disconnection

--- nestkernel/stimulating_backend_mpi.cpp
+++ nestkernel/stimulating_backend_mpi.cpp
@@ -277,6 +277,7 @@ nest::StimulatingBackendMPI::cleanup()
     {
       bool value[ 1 ] = { true };
       MPI_Send( value, 1, MPI_CXX_BOOL, 0, 2, *std::get< 0 >( it_comm.second ) );
+      MPI_Barrier(*std::get< 0 >( it_comm.second ));
       MPI_Comm_disconnect( std::get< 0 >( it_comm.second ) );
       delete std::get< 0 >( it_comm.second );
       delete std::get< 1 >( it_comm.second );
