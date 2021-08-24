6eaf4c7fd6c18be740009eea3779930db2525136
Kusch Lionel <lionel.kusch@univ-amu.fr>
2021-08-24 10:44:33 +0200

    remove print

--- nestkernel/recording_backend_mpi_stream.cpp
+++ nestkernel/recording_backend_mpi_stream.cpp
@@ -240,5 +240,5 @@ nest::RecordingBackendMPIStream::send_data( const MPI_Comm* comm, const int data
   int shape = { size };
   // Receive the data ( for the moment only spike time )
   MPI_Send( data, shape, MPI_INT, 0, tag, *comm );
-  std::cout<<"NEST :"<<data<<std::endl; std::cout.flush();
+//  std::cout<<"NEST :"<<data[0]<<std::endl; std::cout.flush();
 }
